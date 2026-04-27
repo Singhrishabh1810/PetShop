2c5624d97a340351cabfac31578ca3a1f656e288
riya-pawar18 <riyapawar941@gmail.com>
2026-04-27 10:45:37 +0530

    Modified repo

--- src/main/java/com/cg/repo/PetRepository.java
+++ src/main/java/com/cg/repo/PetRepository.java
@@ -1,9 +1,10 @@
 package com.cg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.entity.Pet;
-
+@Repository
 public interface PetRepository extends JpaRepository<Pet, Integer>
 {
 
--- src/main/java/com/cg/repo/SupplierRepository.java
+++ src/main/java/com/cg/repo/SupplierRepository.java
@@ -1,9 +1,10 @@
 package com.cg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.entity.Supplier;
-
+@Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 
 }
