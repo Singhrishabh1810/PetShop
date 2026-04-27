3162bbeaf40a9a10ef936a264677f592c3124b3f
PhantommGeeK <devansh.22b1541054@abes.ac.in>
2026-04-27 10:48:48 +0530

    Added @Repository

--- src/main/java/com/cg/repo/CustomersRepository.java
+++ src/main/java/com/cg/repo/CustomersRepository.java
@@ -1,9 +1,11 @@
 package com.cg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.entity.Customers;
 
+@Repository
 public interface CustomersRepository extends JpaRepository<Customers, Integer> {
 
 }
--- src/main/java/com/cg/repo/VaccinationRepository.java
+++ src/main/java/com/cg/repo/VaccinationRepository.java
@@ -1,9 +1,11 @@
 package com.cg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.entity.Vaccination;
 
+@Repository
 public interface VaccinationRepository extends JpaRepository<Vaccination,Integer>  {
 
 }
